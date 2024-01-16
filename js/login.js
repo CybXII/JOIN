6d72b086081bdd3114641d23a9ea464a41ef65b9
Lukas Nolting <lukas.nolting@icloud.com>
2024-01-16 21:27:30 +0100

    register & login

--- js/login.js
+++ js/login.js
@@ -1,24 +1,22 @@
+function registerComplete() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const msg = urlParams.get("msg");
 
-
-function registerComplete(){
-
-
-const urlParams = new URLSearchParams(window.location.search);
-const msg = urlParams.get("msg");
-
-if (msg) {
-  msgBox = document.getElementById('msgBox');
-  msgBox.innerHTML = msg;
-}
-// else display none
+  if (msg) {
+    msgBox = document.getElementById("msgBox");
+    msgBox.innerHTML = msg;
+  }
+  // else display none
 }
 
-function login(){
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
-    let user = users.find( u => u.email == email.value && u.password == password.value);
-    console.log(user);
-    if(user){
-        console.log("user gefunden");
-    }
+function login() {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  let user = users.find(
+    (u) => u.email == email.value && u.password == password.value
+  );
+  console.log(user);
+  if (user) {
+    console.log("user gefunden");
+  }
 }
