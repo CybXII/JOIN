9dc1a1c0e27463d487f01600e0cbb5bd850f463c
Lukas Nolting <lukas.nolting@icloud.com>
2024-01-16 21:26:07 +0100

    register & login

---
+++ js/register.js
@@ -0,0 +1,9 @@
+let users = [{ email: "max@test.de", password: "test123" }];
+
+function addUser() {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  users.push({ email: email.value, password: password.value });
+  // Weiterleitung zu Loginseite + Nachricht anzeigen erfolgreiche Registrierung
+  window.location.href = "login.html?msg=Du hast Dich erfolgreich registriert";
+}
