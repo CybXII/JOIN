0c181242f818ce511600f1846ea6e15f15893a58
Lukas Nolting <lukas.nolting@icloud.com>
2024-01-20 19:39:56 +0100

    cleaning up

--- js/login.js
+++ js/login.js
@@ -1,3 +1,44 @@
+
+// Login-Screen
+
+document.addEventListener("DOMContentLoaded", function () {
+  const email_div = document.getElementById("parent_email");
+  const email_input = document.getElementById("email");
+  const password_div = document.getElementById("parent_password");
+  const password_input = document.getElementById("password");
+
+  email_input.addEventListener("blur", function () {
+    // Entferne die Klasse, wenn das Inputfeld den Fokus verliert
+    email_div.classList.remove("aktive");
+  });
+
+  email_input.addEventListener("focus", function () {
+    // Füge die Klasse hinzu, wenn das Inputfeld den Fokus hat
+    email_div.classList.add("aktive");
+  });
+
+  password_input.addEventListener("blur", function () {
+    // Entferne die Klasse, wenn das Inputfeld den Fokus verliert
+    password_div.classList.remove("aktive");
+  });
+
+  password_input.addEventListener("focus", function () {
+    // Füge die Klasse hinzu, wenn das Inputfeld den Fokus hat
+    password_div.classList.add("aktive");
+  });
+});
+
+function move() {
+  setTimeout(() => {
+    document.getElementById("logo_container").classList.remove("background");
+    document.getElementById("join_logo").classList.remove("background");
+    document.getElementById("logo_container").classList.remove("big_size");
+  }, 500);
+}
+
+
+// Register & Login
+
 function registerComplete() {
   const urlParams = new URLSearchParams(window.location.search);
   const msg = urlParams.get("msg");
