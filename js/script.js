a3193147c029fef185a1ea9143816b7c82bdbc15
Lukas Nolting <lukas.nolting@icloud.com>
2024-01-20 19:54:44 +0100

    render content functions

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+// Toggle Classlist "frame-10-active" & "frame-10"
+
+function renderSummary(){
+  document.getElementById("content").innerHTML = `Summary`;
+}
+function renderAddTask(){
+  document.getElementById("content").innerHTML = `Add Task`;
+}
+function renderBoard(){
+  document.getElementById("content").innerHTML = `Board`;
+}
+function renderContacts(){
+  document.getElementById("content").innerHTML = `Contacts`;
+}
+
+// Toggle Classlist "font-active" & "font" // ggfs. Klassennamen umbenennen?
+
+function renderPrivacyPolicy(){
+  document.getElementById("content").innerHTML = `Privacy Policy`;
+}
+function renderLegalNotice(){
+  document.getElementById("content").innerHTML = `Legal Notice`;
+}
