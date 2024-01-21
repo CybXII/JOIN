9253a8818d10c5445bb0407d0e95a3f2ad623185
Lukas Nolting <lukas.nolting@icloud.com>
2024-01-21 16:20:44 +0100

    register complete text

--- js/login.js
+++ js/login.js
@@ -1,34 +1,4 @@
 
-// Login-Screen
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const email_div = document.getElementById("parent_email");
-//   const email_input = document.getElementById("email");
-//   const password_div = document.getElementById("parent_password");
-//   const password_input = document.getElementById("password");
-
-//   email_input.addEventListener("blur", function () {
-//     // Entferne die Klasse, wenn das Inputfeld den Fokus verliert
-//     email_div.classList.remove("aktive");
-//   });
-
-//   email_input.addEventListener("focus", function () {
-//     // Füge die Klasse hinzu, wenn das Inputfeld den Fokus hat
-//     email_div.classList.add("aktive");
-//   });
-
-//   password_input.addEventListener("blur", function () {
-//     // Entferne die Klasse, wenn das Inputfeld den Fokus verliert
-//     password_div.classList.remove("aktive");
-//   });
-
-//   password_input.addEventListener("focus", function () {
-//     // Füge die Klasse hinzu, wenn das Inputfeld den Fokus hat
-//     password_div.classList.add("aktive");
-//   });
-// });
-
-
 // WIEDER EINKOMMENTIEREN!!!!
 
 // function move() {
@@ -53,7 +23,6 @@ function registerComplete() {
   // else display none
 }
 
-// let users = [{ email: "max@test.de", password: "test123" }];
 
 function login() {
   let email = document.getElementById("email");
