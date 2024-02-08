2dfb9952b5b8d9d43244b23f9217eb9ab6d6441d
Lukas Nolting <lukas.nolting@icloud.com>
2024-02-08 20:00:38 +0100

    logoutbox

--- js/script.js
+++ js/script.js
@@ -10,37 +10,38 @@ function renderSummary() {
   setAmounts();
 
   // Filtern und sortieren der Daten
-let filteredDueDates = tasks
-  .filter((task) => task.prio === "urgent" && task.categoryboard !== "done")
-  .map((task) => new Date(task.dueDate));
-filteredDueDates.sort((a, b) => a - b);
+  let filteredDueDates = tasks
+    .filter((task) => task.prio === "urgent" && task.categoryboard !== "done")
+    .map((task) => new Date(task.dueDate));
+  filteredDueDates.sort((a, b) => a - b);
 
-// Geringstes Datum extrahieren
-let smallestDueDate = filteredDueDates[0];
+  // Geringstes Datum extrahieren
+  let smallestDueDate = filteredDueDates[0];
 
-// Funktion zur Umwandlung des Datums in das gewünschte Format
-function formatDate(date) {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const month = months[date.getMonth()];
-  const day = date.getDate();
-  const year = date.getFullYear();
-  return `${month} ${day}, ${year}`;
-}
+  // Funktion zur Umwandlung des Datums in das gewünschte Format
+  function formatDate(date) {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const month = months[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month} ${day}, ${year}`;
+  }
 
-  document.getElementById("summ-upcoming").innerHTML = formatDate(smallestDueDate);
+  document.getElementById("summ-upcoming").innerHTML =
+    formatDate(smallestDueDate);
   document.getElementById("summ-tasks-todo").innerHTML = tasksTodo;
   document.getElementById("summ-tasks-done").innerHTML = tasksDone;
   document.getElementById("summ-tasks-urgent").innerHTML = tasksUrgent;
@@ -151,27 +152,29 @@ function renderGreetingTime() {
 
 function greetSummaryAnimation() {
   setTimeout(() => {
-    document.getElementById("frame-69_responsiv").classList.remove("frame-69_responsiv_background");
+    document
+      .getElementById("frame-69_responsiv")
+      .classList.remove("frame-69_responsiv_background");
     document.getElementById("frame-69_responsiv").classList.add("transparent");
   }, 2000);
-
-
 }
+
+// function openLogOutBox() {
 
 function openLogOutBox() {
-  let logOutBox = document.getElementById('LogOutBoxCSS');
-  let isVisible = logOutBox.style.display === 'block';
-  logOutBox.style.display = isVisible ? 'none' : 'block';
-}
-
-window.addEventListener("click", function (e) {
-  if (document.getElementById("openLogOutBox").contains(e.target)) {
-    document.getElementById("LogOutBoxCSS").classList.remove("d-none");
+  let logoutBox = document.getElementById("LogOutBoxCSS");
+  if (!logoutBox.classList.contains("d-none")) {
+    logoutBox.classList.add("d-none");
   } else {
-    document.getElementById("LogOutBoxCSS").classList.add("d-none");
+    logoutBox.classList.remove("d-none");
+    window.addEventListener("click", function (e) {
+      if (document.getElementById("openLogOutBox").contains(e.target)) {
+      } else {
+        document.getElementById("LogOutBoxCSS").classList.add("d-none");
+      }
+    });
   }
-});
-
+}
 
 // document.addEventListener('DOMContentLoaded',function() {
 // document.getElementById('openLogOutBox').addEventListener('click', function(event) {
