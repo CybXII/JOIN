9ea617b0732910536e2925f9741405d183498bca
CybXII <luft.luft86@googlemail.com>
2024-02-25 18:56:07 +0100

    -adjusted function assign to

--- js/board.js
+++ js/board.js
@@ -209,6 +209,7 @@ function openCardContainer() {
 function closeCardContainer() {
   document.getElementById("card-background").classList.add("d-none");
   document.body.classList.remove("background-fixed");
+  document.getElementById("card-background").innerHTML ="";
 }
 
 
@@ -312,7 +313,7 @@ function openEditAssignTo(){
     // Überprüfe, ob die Klasse "visible" nicht enthalten ist, füge sie hinzu
     if (!assingBox.classList.contains("visible")) {
       assingBox.classList.add("visible");
-      renderAssignedUserAddTask();
+      renderAssignedUserAddTask("edit-");
     } else {
       // Wenn "visible" enthalten ist, entferne es
       assingBox.classList.remove("visible");
@@ -325,4 +326,25 @@ function openEditAssignTo(){
         window.removeEventListener("click", arguments.callee); // Entferne den Event-Listener nach Ausführung
       }
     });
+}
+
+function openEditCategory() {
+  let categoryBox = document.getElementById("edit-list2");
+  let categoryInput = document.getElementById("edit-category-list2");
+
+  if (!categoryBox.classList.contains("visible")) {
+    categoryBox.classList.add("visible");
+    categoryInput.setAttribute("disabled", "");
+    renderAssignedUserAddTask();
+  } else {
+    categoryBox.classList.remove("visible");
+    categoryInput.removeAttribute("disabled", "");
+  }
+
+  window.addEventListener("click", function (e) {
+    if (!categoryBox.contains(e.target)) {
+      categoryBox.classList.remove("visible");
+      window.removeEventListener("click", arguments.callee);
+    }
+  });
 }
